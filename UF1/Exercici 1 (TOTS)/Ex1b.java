ede06b9f102d408d932e420deebb702e6f29db5c
albertmvm <95416837+albertmvm@users.noreply.github.com>
2022-10-25 13:16:48 +0200

    Add files via upload

---
+++ UF1/Exercici 1 (TOTS)/Ex1b.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementacio.java;
+
+public class Ex1b {
+    
+    private static final String MSG_1 = ("Mi nombre és, Albert Martinez Ortega");
+
+    public static void main(String[] args) {
+        
+        System.out.println(MSG_1);
+
+    }   
+}
---
+++ UF1/Exercici 1 (TOTS)/Ex1c.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package implementacio.java;
+
+public class Ex1c {
+    
+    private static final String MSG_1 = ("Mi nombre és Albert");
+    private static final String MSG_2 = ("Mi apellido Martinez");
+
+    
+    
+    public static void main(String[] args) {
+        
+        System.out.println(MSG_1);
+        System.out.println(MSG_2);
+    }   
+}
+
