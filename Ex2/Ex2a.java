431a6e989dec05a619da49740d731d3905c38e2a
albertmvm <95416837+albertmvm@users.noreply.github.com>
2022-10-28 14:01:26 +0200

    Add files via upload

---
+++ Ex2/Ex2a.java
@@ -0,0 +1,13 @@
+package implementacio;
+
+//Albert Martínez Ortega
+public class Ex2a {
+    
+    private static final String MSG_1 = ("Hola Món");
+    
+    public static void main(String[] args){
+       
+        System.out.println(MSG_1);
+    }
+    
+}
---
+++ Ex2/Ex2b.java
@@ -0,0 +1,22 @@
+package implementacio.java;
+import java.util.Scanner;
+
+
+public class Ex2b {
+    
+    private static final String MSG_1 = ("Hola Món");
+    //Albert Martinez Ortega
+    //Nom:
+    //Cognoms:
+    //INS Manuel Vázquez Montalbán
+    //Data d’edició:
+    //Nom del cicle formatiu:
+    //Nom del mòdul:
+
+    public static void main(String[] args){
+       
+        System.out.println(MSG_1);
+
+    }
+    
+}
